cbed8b282fe1ba571a8248b2fe4469dfff5a8ec9
Dave Richer <davericher@gmail.com>
2023-09-18 00:56:39 +0000

    Boilerplate

--- services/userGroupsService.js
+++ services/userGroupsService.js
@@ -4,8 +4,7 @@ const UserGroups = require('../models/UserGroups');
 class UserGroupsService {
   static async createUserGroup(data) {
     try {
-      const userGroupId = await UserGroups.query(knex).insert(data);
-      return userGroupId;
+      return await UserGroups.query(knex).insert(data);
     } catch (error) {
       console.error('Error creating user group:', error);
       throw error;
@@ -14,8 +13,7 @@ class UserGroupsService {
 
   static async getUserGroupById(id) {
     try {
-      const userGroup = await UserGroups.query(knex).findById(id);
-      return userGroup;
+      return await UserGroups.query(knex).findById(id);
     } catch (error) {
       console.error('Error fetching user group:', error);
       throw error;
@@ -24,11 +22,7 @@ class UserGroupsService {
 
   static async updateUserGroup(id, data) {
     try {
-      const updatedUserGroup = await UserGroups.query(knex).patchAndFetchById(
-        id,
-        data,
-      );
-      return updatedUserGroup;
+      return await UserGroups.query(knex).patchAndFetchById(id, data);
     } catch (error) {
       console.error('Error updating user group:', error);
       throw error;
@@ -37,8 +31,7 @@ class UserGroupsService {
 
   static async deleteUserGroup(id) {
     try {
-      const rowsDeleted = await UserGroups.query(knex).deleteById(id);
-      return rowsDeleted;
+      return await UserGroups.query(knex).deleteById(id);
     } catch (error) {
       console.error('Error deleting user group:', error);
       throw error;
